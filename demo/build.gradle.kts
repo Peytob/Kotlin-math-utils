ad0fca098b003df330675d4229a103a5aa93c345
Roman Reutov <peytob23@gmail.com>
2023-12-15 14:50:01 +0300

    Readme

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
-    `java-library`
+    application
 }
 
 group = "dev.peytob.math"
@@ -16,6 +16,10 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClassName = "dev.peytob.math.demo.MainKt"
 }
 
 tasks.test {
